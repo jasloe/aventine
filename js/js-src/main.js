48a13e63733d01829e0d8cf53a73a6b496569d7d
Jason Loeffler <j@minorscience.com>
2020-07-15 14:57:42 -0400

    pinned highwater mark

--- js/js-src/main.js
+++ js/js-src/main.js
@@ -1,2 +1,6 @@
 // This should be a preprocess function at *.theme
-document.querySelector('.field-page-internal-link > a').innerHTML = '<span></span>'
+var el = document.querySelectorAll('.field-page-internal-link > a');
+
+for (i = 0; i < el.length; ++i) {
+  el[i].innerHTML = '<span></span>';
+}
--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-document.querySelector(".field-page-internal-link > a").innerHTML="<span></span>";
+var el=document.querySelectorAll(".field-page-internal-link > a");for(i=0;i<el.length;++i)el[i].innerHTML="<span></span>";
 //# sourceMappingURL=maps/main.js.map
