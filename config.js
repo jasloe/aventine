100b4655b4ff9b422e0e4aeb1cae904ee2e2246f
Jason Loeffler <j@minorscience.com>
2021-01-20 16:30:24 -0500

    restore events to home page

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
 module.exports = {
   browserSync: {
-    hostname: "local.aar.local",
+    hostname: "local.academy.local",
     port: 8080,
     openAutomatically: true,
     reloadDelay: 50,
     injectChanges: true,
-    notify: true
+    notify: true,
   },
 
   drush: {
     enabled: false,
     alias: {
       css_js: "drush @SITE-ALIAS cc css-js",
-      cr: "drush @SITE-ALIAS cr"
-    }
+      cr: "drush @SITE-ALIAS cr",
+    },
   },
 
   twig: {
-    useCache: false
-  }
+    useCache: false,
+  },
 };
