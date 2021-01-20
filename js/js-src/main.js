096f08f1354b08d3ae5ebb022af78217e256b2a8
Jason Loeffler <j@minorscience.com>
2021-01-19 19:37:32 -0500

    js for trouble in elsewhere

--- js/js-src/main.js
+++ js/js-src/main.js
@@ -1,58 +1,38 @@
 // This should be a preprocess function at *.theme
-var el = document.querySelectorAll('.field-page-internal-link > a');
-var quickNewsLinks = document.querySelectorAll('.field-quick-news-links a')
+// var el = document.querySelectorAll('.field-page-internal-link > a');
+// var quickNewsLinks = document.querySelectorAll('.field-quick-news-links a')
 
-for (i = 0; i < el.length; ++i) {
-  el[i].innerHTML = '<span></span>';
-}
+// for (i = 0; i < el.length; ++i) {
+//   el[i].innerHTML = '<span></span>';
+// }
 
-for (i = 0; i < quickNewsLinks.length; ++i) {
-  quickNewsLinks[i].innerHTML = '<span></span>';
-}
+// for (i = 0; i < quickNewsLinks.length; ++i) {
+//   quickNewsLinks[i].innerHTML = '<span></span>';
+// }
 
-(function ($) {
-  // add markup to region-header
-  var $window = $(window);
-  var $hamburger = $(".hamburger");
-  var $body = $("body");
-  var $overlay = $("#overlay");
+// Card links
+const card = document.querySelectorAll(".path-news-far-afield .card-inner")
+const externalLink = document.querySelectorAll(".field-quick-news-links a")
 
-  function resize() { // resize function
-    if ($window.width() < 1024) {
-      $body.addClass("mobile");
-    } else {
-      $body.removeClass("mobile");
-      $body.removeClass("is-open");
-      $hamburger.removeClass("is-active");
-      $overlay.removeClass("is-open");
-    }
-  };
+document.addEventListener("DOMContentLoaded", function () {
 
-  $hamburger.on("click", function () { // hamburger clicked
-    $hamburger.toggleClass("is-active"); // change hamburger state
-    $body.toggleClass("no-scroll"); // disable scrolling
-    $body.toggleClass("is-open"); // add class to hide menu
-    $overlay.toggleClass("is-open");
-  });
+  for (i = 0; i < card.length; ++i) {
+    card[i].addEventListener("click", function(event) {
+      const isTextSelected = window.getSelection().toString();
+      if (!isTextSelected) {
+        // externalLink[i].click();
 
+      }
+    })
+  }
+});
 
-  $window
-    .resize(resize)
-    .trigger('resize');
-
-})(jQuery);
-
-// This should be a preprocess function at *.theme
-var el = document.querySelectorAll('.field-page-internal-link > a');
-var quickNewsLinks = document.querySelectorAll('.field-quick-news-links a')
-
-for (i = 0; i < el.length; ++i) {
-  el[i].innerHTML = '<span></span>';
-}
-
-for (i = 0; i < quickNewsLinks.length; ++i) {
-  quickNewsLinks[i].innerHTML = '<span></span>';
-}
+// function handleClick(event) {
+//   const isTextSelected = window.getSelection().toString();
+//   if (!isTextSelected) {
+//     externalLink.click();
+//   }
+// }
 
 (function ($) {
   // add markup to region-header
