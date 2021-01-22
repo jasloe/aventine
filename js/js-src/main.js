ced826d06db8a3f1dc6b9b6a1b9b4cccfb235503
Jason Loeffler <j@minorscience.com>
2021-01-21 19:05:55 -0500

    zoom layout and js

--- js/js-src/main.js
+++ js/js-src/main.js
@@ -1,6 +1,7 @@
 // This should be a preprocess function at *.theme
 var el = document.querySelectorAll('.field-page-internal-link > a');
 var quickNewsLinks = document.querySelectorAll('.field-quick-news-links a')
+var zoomLinks = document.querySelectorAll('.field-events-zoom-link a')
 
 for (i = 0; i < el.length; ++i) {
   el[i].innerHTML = '<span></span>';
@@ -8,6 +9,10 @@ for (i = 0; i < el.length; ++i) {
 
 for (i = 0; i < quickNewsLinks.length; ++i) {
   quickNewsLinks[i].innerHTML = '<span></span>';
+}
+
+for (i = 0; i < zoomLinks.length; ++i) {
+  zoomLinks[i].innerHTML = '<span></span>';
 }
 
 // Card links
