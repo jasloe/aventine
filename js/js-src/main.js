7d515b89af2a660329c454ebb91d403ed98543a5
Jason Loeffler <j@minorscience.com>
2021-02-02 18:00:21 -0500

    fix zoom card links

--- js/js-src/main.js
+++ js/js-src/main.js
@@ -1,7 +1,7 @@
 // This should be a preprocess function at *.theme
 var el = document.querySelectorAll('.field-page-internal-link > a');
 // var quickNewsLinks = document.querySelectorAll('.field-quick-news-links a')
-var zoomLinks = document.querySelectorAll('.field-events-zoom-link a')
+// var zoomLinks = document.querySelectorAll('.field-events-zoom-link a')
 
 for (i = 0; i < el.length; ++i) {
   el[i].innerHTML = '<span></span>';
@@ -11,9 +11,9 @@ for (i = 0; i < el.length; ++i) {
 //   quickNewsLinks[i].innerHTML = '<span></span>';
 // }
 
-for (i = 0; i < zoomLinks.length; ++i) {
-  zoomLinks[i].innerHTML = '<span></span>';
-}
+// for (i = 0; i < zoomLinks.length; ++i) {
+//   zoomLinks[i].innerHTML = '<span></span>';
+// }
 
 // Card links
 // const card = document.querySelectorAll(".path-news-far-afield .card-inner")
