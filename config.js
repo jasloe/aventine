b9075d7aac0ba372ec67ff62fdc926edfa735a61
jasloe <jason@minorscience.com>
2023-04-03 22:53:17 -0400

    feat: pushing mc to production

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   browserSync: {
-    hostname: "http://local.d9-aarome.local/",
+    hostname: "http://aarome.lndo.site",
     port: 8080,
     openAutomatically: true,
     reloadDelay: 50,
